a92d3e6c71eec4384f76fbe385a509d462502bb2
William <william_monroy777@hotmail.com>
2020-12-28 14:58:23 -0500

    primer orden

--- main.py
+++ main.py
@@ -14,8 +14,26 @@ with open('medidas.csv','r') as archivo:
 
 medidas.sort(reverse=True)
 
+actual = 0
+cortes = []
 
+corte = []
+acum = 0
 
+for var in range(len(medidas)):
+    if acum + medidas[var] <= varilla:
+        corte.append(medidas[var])
+        acum += medidas[var]
+    else:
+        acum = 0
+        print(corte)
+        cortes.append(corte)
+        corte = []
 
+for i in cortes:
+    suma=0
+    for e in i:
+        suma += e
+    print('suma:', suma,'aprov:',str(round(suma*100/600,2))+'%', 'piezas:', i)
     
     
