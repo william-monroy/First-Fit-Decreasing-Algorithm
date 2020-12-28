68fa790266c5c57a45b44c94142c06f583c33a8d
William <william_monroy777@hotmail.com>
2020-12-28 12:28:01 -0500

    lectura de medidas

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
 
+varilla = 600
+medidas = []
+
+with open('medidas.csv','r') as archivo:
+    file = archivo.read().split('\n')
+    for elm in file:
+        read = elm.split(',')
+        read[0] = float(read[0])
+        read[1] = int(read[1])
+        for i in range(read[1]):
+            medidas.append(read[0])
+
+
+medidas.sort(reverse=True)
+
+
+
+
+    
+    
